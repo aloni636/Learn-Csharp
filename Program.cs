e0c3e72b279787dfe9bcd8cebe73eabe73f8e5fd
aloni636 <elyasaf.aloni@gmail.com>
2025-01-28 20:25:34 +0200

    Added ControlStructures, Typecasting and Functions

--- Program.cs
+++ Program.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Linq;  // Language-Integrated Query, allows SQL like expressions on linq enabled data structures
 using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Learning.csharp
 {
     public class Program
     {
-        public static void Syntax()
+        private static readonly Random GlobalRandom = new Random();
+        public static void DataStructures()
 
         {
             Console.WriteLine("Hello, World!");
@@ -77,9 +79,71 @@ namespace Learning.csharp
             Console.WriteLine("\nhashSet union: { "+string.Join(", ", hashSet.Union(setRange).Select((i) => i.ToString()))+" }");
             Console.WriteLine("hashSet difference: { "+string.Join(", ", hashSet.Except(setRange).Select((i) => i.ToString()))+" }");
         }
+        public static void ControlStructures()
+        {
+            foreach (int i in Enumerable.Range(0, 10))
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Even: " + i);
+                }
+                else
+                {
+                    Console.WriteLine("Odd: " + i);
+                }
+            }
+        }
+        public static void Typecasting()
+        {
+            Console.WriteLine("Provide an integer to parse:");
+            string userInput = Console.ReadLine() ?? "";
+            if (int.TryParse(userInput, out int tryInt))  // TODO: Learn how to use out keyword in custom methods
+            {
+                Console.WriteLine("Parsed int: " + tryInt);
+                string tryIntAsString = tryInt.ToString();
+                Console.WriteLine("Parsed int Hex: " + tryInt.ToString("X"));  // X is a format identifier, see: https://learn.microsoft.com/en-us/dotnet/api/system.int32.tostring?view=net-9.0
+                long tryIntAsLong = (long)tryInt;
+                byte[] intBytes = BitConverter.GetBytes(tryInt);
+                byte[] longBytes = BitConverter.GetBytes(tryIntAsLong);
+                Console.WriteLine("Parsed int bytes: " + BitConverter.ToString(intBytes));
+                Console.WriteLine("Parsed int as long bytes: " + BitConverter.ToString(longBytes));
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse int");
+            }
+        }
+        public static void Functions()
+        {
+            // default and optional parameters, keywords args, variable amount of arguments
+            int MyMethod(int positional, int optional=4, int? optionalNull = null, params int[] numbers)
+            {
+                return positional + optional + (optionalNull ?? 0) + numbers.Sum();
+            }
+            Console.WriteLine("MyMethod with positional, keyword, and variadic args: " + MyMethod(1, optionalNull:2, optional:3, numbers: [4, 5]));
+            bool OutMethod(out int outParam)
+            {
+                int randInt = GlobalRandom.Next(0,2);
+                outParam = randInt;
+                return (randInt == 0);
+            }
+            if (OutMethod(out int outParam))
+            {
+                Console.WriteLine("OutMethod returned true with outParam: " + outParam);
+            }
+            else
+            {
+                Console.WriteLine("OutMethod returned false with outParam: " + outParam);
+            }
+        }
         public static void Main(string[] args)
         {
-            Syntax();
+
+            DataStructures();
+            Console.WriteLine('\n'+new string('-', 50)+'\n');
+            ControlStructures();
+            Console.WriteLine('\n'+new string('-', 50)+'\n');
+            Typecasting();
         }
     }
 }
