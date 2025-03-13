bae7fc07321d2e8b5852d4614bec031c624afb15
aloni636 <elyasaf.aloni@gmail.com>
2025-03-13 16:25:43 +0200

    Added doxygen style docs

--- Projects/CppInterop/HeaderOnlyLib.h
+++ Projects/CppInterop/HeaderOnlyLib.h
@@ -4,47 +4,27 @@
 #include <algorithm>
 #include <cmath>
 
-// TODO: Convert to function
-
-// NOTE: Templates cannot be separated into declaration and implementation
-//class Histogram {
-//private:
-//	template <typename T>
-//	T roundToRange(T value, T min, T max, int bin) {
-//
-//	}
-//public:
-//	int bins;
-//	Histogram(int bins) {
-//		this->bins = bins;
-//	};
-//	template <typename T>
-//	std::map<T, int> compute(std::vector<T> &vec) {
-//		std::map<T, int> outHistogram;
-//		auto minMax = std::minmax_element(vec.begin(), vec.end());
-//		T min = *minMax.first;
-//		T max = *minMax.second;
-//		for (T element : vec) {
-//			// linearly remap to between 0 and 1
-//			// scale to 0 and bin size
-//			// round to nearest
-//			// scale back linearly to between min and 
-//			auto v1 = (std::clamp(element, min, max) - min);
-//			auto roundedElement = std::round(
-//				(
-//					v1 / ( max - min)
-//					) * bins
-//			);
-//			roundedElement = ((roundedElement / bins) * (max - min)) + min;
-//			T outElement = static_cast<T>(roundedElement);
-//			outHistogram[outElement]++;
-//		}
-//		return outHistogram;
-//	};
-//};
-
-// {1,2,3,4,5} bins = 3 {=> 1 => 2, 3 => 2, 5 => 1}
-// {1,2,3,4,5,6,7} bins = 3 => {1 => 3, 4 => 3, 7 => 1}
+/**
+ * @brief Computes the occurrence of values in a vec and bucket them into equally sized bins.
+ * @tparam T The type of each vector element.
+ * @param vec The input vector to base the histogram upon.
+ * @param bins The number of bins in the histogram.
+ * @return A map of bin and count. The range of bin i is from i to (not including) i+1.
+ *
+ * ```
+ * std::vector<int> vec = {1,2,3,4,5};
+ * int bins = 3;
+ * auto result = computeHistogram(vec, bins);
+ *
+ * // Expected output:
+ * // {1 => 2, 3 => 2, 5 => 1}
+ * std::vector<int> vec = {1,2,3,4,5,6,7};
+ * auto result = computeHistogram(vec, bins);
+ *
+ * // Expected output:
+ * // {1 => 3, 4 => 3, 7 => 1}
+ * ```
+ */
 template <typename T>
 std::map<double, int> computeHistogram(std::vector<T>& vec, int bins) {
     std::map<double, int> out;
