0ad50ca9f8924c5cacc3258373be2f0210f3dd34
aloni636 <elyasaf.aloni@gmail.com>
2025-03-18 01:27:09 +0200

    win32: Made text center aligned

--- WinApiMinimal/Main.cpp
+++ WinApiMinimal/Main.cpp
@@ -1,6 +1,10 @@
 #include <windows.h>
 #include <sstream>
 #include "windowsx.h"  // for GET_X_LPARAM and GET_Y_LPARAM
+
+#include <dwmapi.h> // Required for rounded corners
+#pragma comment(lib, "Dwmapi.lib") // Link the library
+
 // TODO (1): Create and Show a Window(Essential for TSF)
 // TODO (2): Explore the Windows Message System
 // TODO (3): Handling Basic Events(Key & Mouse Input)
@@ -21,11 +25,18 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         hFont = CreateFont(18, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE,
             DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS,
             DEFAULT_QUALITY, FF_SWISS, L"Consolas");
+        // Enable rounded corners (Windows 11+)
+         DWM_WINDOW_CORNER_PREFERENCE preference = DWMWCP_ROUNDSMALL;  // Control window rounding style
+         DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &preference, sizeof(preference));
         return 0;
     }
+    case WM_SIZE:
+        InvalidateRect(hwnd, NULL, TRUE); // Request a redraw to keep text centered when resizing
+        return 0;
     case WM_CLOSE:  // when closed
         PostQuitMessage(0);
         return 0;
+
 
     // Keyboard Events
     case WM_KEYDOWN: { // Capture key presses
@@ -34,6 +45,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         return 0;
     }
     
+
     // Mouse Events
     case WM_LBUTTONDOWN: {
         POINT pt;
@@ -52,19 +64,29 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         return 0;
     }
 
+
     // Painting Events
     case WM_PAINT: { // Redraw the window and display text
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+        HDC hdc = BeginPaint(hwnd, &ps);  // hdc = Handle to Display device Context
         
         // Text Appearance
         SelectObject(hdc, hFont); // Apply custom font
         SetTextColor(hdc, RGB(255, 255, 255)); // White text color
         SetBkMode(hdc, TRANSPARENT); // Transparent background for text
 
+        // Center Text
+        RECT rect;
+        GetClientRect(hwnd, &rect); // Get window size
+        // upper-left point is (0,0)
+        int centerX = rect.right / 2;
+        int centerY = rect.bottom / 2;
+        
+        SetTextAlign(hdc, TA_CENTER);
+
         // Text Content
-        TextOut(hdc, 20, 20, lastEventReport, wcslen(lastEventReport));
-        TextOut(hdc, 20, 40, lastMousePositionReport, wcslen(lastMousePositionReport));
+        TextOut(hdc, centerX, centerY-20, lastEventReport, wcslen(lastEventReport));
+        TextOut(hdc, centerX, centerY+0, lastMousePositionReport, wcslen(lastMousePositionReport));
         
         EndPaint(hwnd, &ps);
         return 0;
